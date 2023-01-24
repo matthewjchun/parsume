952ab9b6ce65861a31dad7a05ef6725fbf1bf0f6
unknown <matthewchun.18@gmail.com>
2023-01-24 16:40:56 -0500

    playing around with routes

---
+++ backend/routes/home.js
@@ -0,0 +1,4 @@
+const router = require('express').Router();
+let Resume = require('../models/resume.model');
+
+
--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,13 @@ connection.once('open', () => {
     console.log("MongoDB database connection established successfully");
 })
 
+const homeRouter = require('./routes/home');
+const resultsRouter = require('./routes/results');
+const searchRouter = require('./routes/search');
+
+app.use('/', homeRouter);
+app.use('/results', resultsRouter);
+app.use('/search', searchRouter);
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
