6b080128663fe723b3aeb8d68d862f65d9ac06c4
unknown <matthewchun.18@gmail.com>
2023-01-25 23:24:26 -0500

    baseline home page completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import { BrowserRouter, Route } from 'react-router-dom'
+// import 'bootstrap/dist/css/bootstrap.min.css'
+// import { BrowserRouter, Route } from 'react-router-dom'
 import Home from './components/Home/Home'
-import Results from './components/Results/Results'
-import Search from './components/Search/Search'
+// import Results from './components/Results/Results'
+// import Search from './components/Search/Search'
 
 function App() {
   return (
@@ -11,8 +11,8 @@ function App() {
     // <BrowserRouter>
     //   <div>
     //     <Route path="/" exact component={Home} />
-    //     <Route path="/Results" component={Results} />
-    //     <Route path="/Search" component={Search} />
+    //     <Route path="/results" component={Results} />
+    //     <Route path="/search" component={Search} />
     //   </div>
     // </BrowserRouter>
   );
--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
-import { FilePond, File, registerPlugin } from 'react-filepond'
+import { FilePond } from 'react-filepond'
+// File, registerPlugin
+import { Link } from 'react-router-dom'
 import 'filepond/dist/filepond.min.css'
 import './Home.css';
 
@@ -21,6 +23,15 @@ function Home() {
             name="files" /* sets the file input name, it's filepond by default */
             labelIdle='Drag & Drop your Resume or <span class="filepond--label-action">Browse</span>'
         />
+        <button className="Search" type="button" class="btn btn-primary btn-lg btn-block">
+          <svg xmlns="http://www.w3.org/2000/svg" width="25" height="20" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+          </svg> 
+           Search previously submitted resumes  
+        </button>
+        {/* <Link to="/search">
+          <button>Search previously submitted resumes</button>
+        </Link> */}
     </div>
   );
 }
