4b6273da0327692b4d16fe7f7bc7c6bd2c0d3e2a
unknown <matthewchun.18@gmail.com>
2023-01-26 17:04:26 -0500

    basic search page complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 // import 'bootstrap/dist/css/bootstrap.min.css'
 // import { BrowserRouter, Route } from 'react-router-dom'
 import Home from './components/Home/Home'
+import Search from './components/Search/Search';
 // import Results from './components/Results/Results'
 // import Search from './components/Search/Search'
 
 function App() {
   return (
-    <Home></Home>
+    // <Home></Home>
+    <Search></Search>
     // <BrowserRouter>
     //   <div>
     //     <Route path="/" exact component={Home} />
--- frontend/src/components/Search/Search.js
+++ frontend/src/components/Search/Search.js
@@ -1,11 +1,43 @@
 import './Search.css';
+import JSONDATA from './MOCK_DATA.json'
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
+import { useState } from 'react'
 
 function Search() {
+  const [searchTerm, setSearchTerm] = useState('')
+
   return (
-    <div>
-        <h>
-          PARSUME
-        </h>
+    <div className="Format">
+      {/* <div className="Search">
+      <InputGroup className="mb-3">
+        <DropdownButton
+          variant="outline-secondary"
+          title="Category"
+          id="input-group-dropdown-1"
+        >
+          <Dropdown.Item href="#">Name</Dropdown.Item>
+          <Dropdown.Item href="#">Skills</Dropdown.Item>
+        </DropdownButton>
+        <Form.Control id="input-form" aria-label="Text input with dropdown button" />
+      </InputGroup>
+      </div> */}
+      <input className="Search" type="text" placeholder="Search..." onChange={e => {setSearchTerm(e.target.value)}}></input>
+      {JSONDATA.filter((val) => {
+        if (searchTerm == "") {
+          return val
+        } else if (val.first_name.toLowerCase().includes(searchTerm.toLowerCase())) {
+          return val
+        }
+      }).map((val, key) => {
+        return (
+          <div>
+            <p>{val.first_name} </p>
+          </div>
+        )
+      })}
     </div>
   );
 }
